61a20ef1d3e0b24b5440e3b107c32d03ef281722
yuying <xieyuyying993@gmail.com>
2025-11-18 12:20:15 -0800

    update basemap

--- script/script.js
+++ script/script.js
@@ -179,13 +179,10 @@ class MapManager {
     });
 
     L.tileLayer(
-      "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}",
+      "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
       {
-        minZoom: 0,
         maxZoom: 20,
-        attribution:
-          '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        ext: "png",
+        attribution: "&copy; CARTO &copy; OpenStreetMap contributors",
       }
     ).addTo(this.mapWgs);
   }
@@ -420,15 +417,13 @@ class LayerManager {
     const map = this.getMap();
     if (!map) return;
     this.openStreetMap = L.tileLayer(
-      "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}",
+      "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
       {
-        minZoom: 0,
         maxZoom: 20,
-        attribution:
-          '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        ext: "png",
+        attribution: "&copy; CARTO &copy; OpenStreetMap contributors",
       }
-    );
+    ).addTo(map);
+
     this.openStreetMap.addTo(map);
   }
 
